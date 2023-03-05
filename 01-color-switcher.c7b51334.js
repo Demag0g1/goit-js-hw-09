94d0e33f201a16699647d3762027dacaecfc0628
Demag0g1 <112280688+Demag0g1@users.noreply.github.com>
2023-03-05 15:32:09 +0000

    Deploying to gh-pages from @ Demag0g1/goit-js-hw-09@65b2f6d9e88028821ab95447fbc63a78a3376242 🚀

---
+++ 01-color-switcher.c7b51334.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0;var n=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),e.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.c7b51334.js.map
